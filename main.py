af65012906e6a218ee1c4a9955f8d598bc9eefe1
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-01 10:30:09 -0500

    pass thru encoding fot website html parser

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 
-with open("website.html") as file:
+with open("website.html", "r", encoding="utf-8", errors="ignore") as file:
     contents = file.read()
     
 soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
+print(soup.title)
 # print(soup.title.name)
 # print(soup.prettify())
-print(soup)
+# print(soup)

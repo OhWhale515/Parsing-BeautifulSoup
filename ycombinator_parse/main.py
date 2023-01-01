ac89a66165b4b27c76517711db30f976749b6add
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-01 13:43:08 -0500

    article appends

--- ycombinator_parse/main.py
+++ ycombinator_parse/main.py
@@ -10,7 +10,9 @@ article_texts = []
 article_links = []
 for article_tag in articles:
     article_text = article_tag.getText()
+    article_texts.append(article_text)
     article_link = article_tag.get("href")
+    article_links.append(article_link)
     
 article_upvotes = soup.find_all(name="span", class_="score").getText()
 

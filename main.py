278cd4c62e5c77b4c0127924d9b65272aca17f20
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-01 10:39:41 -0500

    created tag for anchors

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ with open("website.html", "r", encoding="utf-8", errors="ignore") as file:
     contents = file.read()
     
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
+# print(soup.title)
 # print(soup.title.name)
 # print(soup.prettify())
 # print(soup)
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.getText())
